4de61eeca7fcf96869e07bdc35008bcaea0a763a
Endekalu-Zemenu <endekaluzemenu2134@gmail.com>
2024-10-26 21:04:17 +0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  app.use(
+    cors({
+      origin: '*', // Allow all origins
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+      credentials: true, // Set to true if you want to allow cookies
+    })
+  );
 }
+
 bootstrap();
